336e0f89713b7ee1433c9ad9f9f9cfd3f6d072fb
Morta <mr.tajik1999@gmail.com>
2025-02-13 19:26:22 +0330

    bla bla

--- js/index.js
+++ js/index.js
@@ -1,37 +1,37 @@
 const notificationBtn = document.querySelector('#notification-btn');
 const alertParagraph = document.querySelector('p');
 alertParagraph.innerHTML = document.visibilityState;
-alertParagraph.innerHTML += "     " + ("Notification" in window)
-let registration;
+alertParagraph.innerHTML += "     " + "Notification is =" + ("Notification" in window)
 
 
+const permission = await Notification.requestPermission();
+const registration = await navigator.serviceWorker.register("./js/sw.js");
 
-if (typeof navigator.serviceWorker !== "undefined") {
-    registration = await navigator.serviceWorker.register("./js/sw.js");
+navigator.serviceWorker.addEventListener("message", (event) => {
+    if (event.data === "activated") {
+        window.location.reload();
+    }
+});
 
-    await registration.showNotification("from outside")
 
-    navigator.serviceWorker.addEventListener("message", (event) => {
-        if (event.data === "activated") {
-            window.location.reload();
-        }
-    });
+try {
+    await registration.showNotification("from outside sw")
+    const n = new Notification("from outside notif api");
+    setTimeout(() => {
+        n.close();
+    }, 3000)
+} catch (err) {
+    alertParagraph.innerHTML += "  we got serious errors--" + err.message;
+    console.log(err)
 }
 
 notificationBtn.addEventListener("click", async () => {
-    const permission = await Notification.requestPermission();
 
     if (permission === "granted") {
         alertParagraph.innerHTML += "    granted";
         registration.active.postMessage({
             type: "notification"
         });
-        try {
-            new Notification("from inside");
-        } catch (err) {
-            alertParagraph.innerHTML += "    " + err.message;
-            console.log(err)
-        }
 
     }
 })
